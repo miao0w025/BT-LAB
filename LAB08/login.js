a126814282f0b1102939c7619df260f201026825
Đỗ Ngọc Cát Tương <n23dcpt055@student.ptithcm.edu.vn>
2025-09-28 00:19:20 +0700

    Create login.js

---
+++ LAB08/login.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loginBtn = document.getElementById("loginBtn");
+  const usernameInput = document.getElementById("username");
+  const passwordInput = document.getElementById("password");
+
+  loginBtn.addEventListener("click", function (event) {
+    event.preventDefault(); // chặn reload trang
+    const username = usernameInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    // Xóa thông báo cũ
+    let oldMsg = document.querySelector(".login-message");
+    if (oldMsg) oldMsg.remove();
+
+    const message = document.createElement("p");
+    message.classList.add("login-message");
+
+    if (!username || !password) {
+      message.textContent = "Vui lòng nhập thông tin";
+      message.style.color = "orange";
+    } else if (username === "nghi" && password === "123456") {
+      message.textContent = "Đăng nhập thành công";
+      message.style.color = "green";
+    } else {
+      message.textContent = "Sai tên đăng nhập hoặc mật khẩu";
+      message.style.color = "red";
+    }
+
+    document.querySelector(".login-container").appendChild(message);
+  });
+});
