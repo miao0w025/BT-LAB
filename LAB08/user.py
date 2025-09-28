171dfdd8e67e3febb184e1527ca6efad4e026e9f
Phuong Nghi <n23dcpt037@student.ptithcm.edu.vn>
2025-09-28 22:08:48 +0700

    Delete LAB08/user.py

--- LAB08/user.py
+++
@@ -1,8 +0,0 @@
-# user.py
-class User:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-    def verify_login(self, username, password):
-        return self.username == username and self.password == password
